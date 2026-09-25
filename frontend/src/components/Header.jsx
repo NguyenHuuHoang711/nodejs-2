b920e41bbc27c556c552221c4fee2a6326212a28
hip21 <hiept674@gmail.com>
2026-09-25 19:57:20 +0700

    update product

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header({ onLogout = () => {} }) {
         to="/"
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
       >
-        <span className="fs-4">MyDrive</span>
+        <span className="fs-4">MyDrivee</span>
       </Link>
       <hr />
 
